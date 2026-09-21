2f7eadbf0d9104aeda1fe36e85fbc084eaca5470
leoneze384-crypto <leoneze384@gmail.com>
2026-09-21 11:49:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='inventario-maquinas-v20-1';
+const C='inventario-maquinas-v20-2';
 const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
